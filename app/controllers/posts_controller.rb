f62f3df12525d753bd37ecc1d670a3d47afd2a0a
sbalgobin94 <sbalgobin94@gmail.com>
2020-09-30 16:58:45 -0400

    changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
     
       private
         def post_params
-          params.require(:post).permit(:title, :description, :image_url)
+          params.require(:post).permit(:title, :description, :image_url, :user_id)
         end
 
 end
