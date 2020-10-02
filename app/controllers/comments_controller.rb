1f13b633fbf0728a3dab6d673b3a1dde726e8846
sbalgobin94 <sbalgobin94@gmail.com>
2020-10-02 18:56:30 -0400

    comments working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,6 +27,6 @@ class CommentsController < ApplicationController
     
       private
         def post_params
-          params.require(:comment).permit(:content, :post_id)
+          params.require(:comment).permit(:content, :post_id, :user_id)
         end
 end
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,3 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :owner
+  attributes :id, :content, :user
 end
